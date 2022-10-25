31b6300fdf7c237bd42b372b97788e054597922e
Juan Victor Ventura Santos <juanvictortimaster@gmail.com>
2022-10-24 22:11:24 -0300

    Creating snake project | JFrame | JPanel

---
+++ src/SnakeGame.java
@@ -0,0 +1,17 @@
+
+public class SnakeGame {
+
+	public static void main(String[] args) {
+		
+		/*
+		 * If you don't plan on using this frame for anything else, 
+		 * you can just instance with new GameFrame()
+		 * 
+		 * like this: new GameFrame()
+		 */
+		
+		GameFrame frame = new GameFrame(); 
+
+	}
+
+}
